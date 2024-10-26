3487e6e60d344381cbd5d85cc73c072b8f88bbe0
AdamITDEV <nguyenminhthong141004@gmail.com>
2024-10-26 22:15:54 +0700

    update carousel

--- src/app/showpage.tsx
+++ src/app/showpage.tsx
@@ -1,156 +1,56 @@
-import Image from 'next/image'
+import Carousel from './components/carousel'
+import Rankings from './components/rankings'
+
+const slides = [
+  {
+    Id: 1,
+    Title: 'React JS',
+    ingredients: 'Tổng Hợp Thư Viện Tích Hợp',
+    photoName: '/images/banner1.png',
+  },
+  {
+    Id: 2,
+    Title: 'Python',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    photoName: '/images/banner2.png',
+  },
+  {
+    Id: 3,
+    Title: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    photoName: '/images/banner3.png',
+  },
+]
+const rating = [
+  {
+    Id: 1,
+    Name: 'React JS',
+    trendingArticles: 'Tổng Hợp Thư Viện Tích Hợp',
+    Avatar: '/images/banner1.png',
+  },
+  {
+    Id: 2,
+    Title: 'Python',
+    trendingArticles: 'Bread with italian olive oil and rosemary',
+    Avatar: '/images/banner2.png',
+  },
+  {
+    Id: 3,
+    Title: 'Focaccia',
+    trendingArticles: 'Bread with italian olive oil and rosemary',
+    Avatar: '/images/banner3.png',
+  },
+]
 
 export default function ShowPage() {
   return (
-    <div>
-      <Carousel />
-    </div>
-  )
-}
-
-const Carousel = () => {
-  return (
-    <div>
-      <div id="default-carousel" className=" mt-10 relative w-full " data-carousel="slide">
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96 ">
-          {/* thêm hidden cho className */}
-          <div className="  duration-700 ease-in-out" data-carousel-item>
-            <Image
-              width={800}
-              height={800}
-              src="/images/banner1.png"
-              className="absolute block  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 border-4 border-gray-900 border-solid rounded-lg"
-              alt="..."
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              width={800}
-              height={800}
-              src="/images/banner2.png"
-              className="absolute block  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 border-4 border-gray-900 border-solid rounded-lg"
-              alt="..."
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              width={800}
-              height={800}
-              src="/images/banner1.png"
-              className="absolute block  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 border-4 border-gray-900 border-solid rounded-lg"
-              alt="..."
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              width={1280}
-              height={367}
-              src="/docs/images/carousel/carousel-4.svg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              width={1280}
-              height={367}
-              src="/docs/images/carousel/carousel-5.svg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-        </div>
-
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 4"
-            data-carousel-slide-to="3"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 5"
-            data-carousel-slide-to="4"
-          ></button>
-        </div>
-
-        <button
-          type="button"
-          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-gray-600 dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-gray-600 dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
+    <div className="items-center">
+      <Carousel slides={slides} />
+      <div className="  relative w-full text-center top-10 border-double border-4 border-[#111827]">
+        Accredited library collection
+      </div>
+      <div className="relative w-full  top-16">
+        <Rankings rating={rating} />
       </div>
     </div>
   )
