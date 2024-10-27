d8c27b73cb1f225bc503f40e7a3c85934d69ab87
AdamITDEV <nguyenminhthong141004@gmail.com>
2024-10-27 16:39:49 +0700

    udapte code

--- src/app/components/rankings.tsx
+++ src/app/components/rankings.tsx
@@ -36,7 +36,7 @@ const Rankings: React.FC<RankingsProps> = ({ rating }) => {
       }`}
     >
       <div className="flex-1">
-        <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-96 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
           <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white flex justify-center ">
             Top #1 <FaArrowRightLong className="mx-3 mt-1" /> #5
           </h5>
@@ -232,7 +232,7 @@ const Rankings: React.FC<RankingsProps> = ({ rating }) => {
         </div>
       </div>
       <div className="flex-1">
-        <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-96 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
           <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white flex justify-center ">
             Top #5 <FaArrowRightLong className="mx-3 mt-1" /> #10
           </h5>
