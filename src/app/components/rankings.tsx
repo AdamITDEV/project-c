48cc2c9794f9dfcd2554f9d3ddf8e9757d54164f
AdamITDEV <nguyenminhthong141004@gmail.com>
2024-10-27 23:13:52 +0700

    check
    avatar

--- src/app/components/rankings.tsx
+++ src/app/components/rankings.tsx
@@ -57,15 +57,15 @@ const Rankings: React.FC<RankingsProps> = ({ rating }) => {
                     href="#"
                     className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
                   >
-                    <div className="w-[10rem] h-[4rem]">
+                    <span className="flex w-16">
                       <Image
                         className="flex-auto rounded-md"
                         src={rating.Avatar}
                         alt={rating.Name}
-                        width={64}
-                        height={64}
+                        width={320}
+                        height={120}
                       />
-                    </div>
+                    </span>
                     <span className="flex-1 ms-3 whitespace-nowrap">{rating.Name}</span>
                     <span className="inline-flex items-center justify-center px-2 py-0.5 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
                       {rating.Label ? ' Master' : ' '}
