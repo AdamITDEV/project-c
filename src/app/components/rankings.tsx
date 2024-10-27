462db38f52ab0ed8d33bdfc00ea541446743f622
AdamITDEV <nguyenminhthong141004@gmail.com>
2024-10-27 22:47:23 +0700

    fix code avatar

--- src/app/components/rankings.tsx
+++ src/app/components/rankings.tsx
@@ -57,7 +57,7 @@ const Rankings: React.FC<RankingsProps> = ({ rating }) => {
                     href="#"
                     className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
                   >
-                    <span className="flex-1 ms-3 whitespace-nowrap">
+                    <span className="flex-1  whitespace-nowrap">
                       <Image
                         className=" rounded-md"
                         src={rating.Avatar}
