c9cece944cd512cfacce64c0d31b188354f3a403
AdamITDEV <nguyenminhthong141004@gmail.com>
2024-11-26 00:19:34 +0700

    -update complete mobile

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -3,11 +3,11 @@ import TopMenu from '../_components/navmenu'
 import ShowPage from '../showpage'
 export default function Home() {
   return (
-    <div className=" font-[family-name:var(--font-geist-sans)]">
+    <div className=" font-[family-name:var(--font-geist-sans)] bg-white dark:bg-black">
       <header className="z-[1000] relative">
         <TopMenu />
       </header>
-      <main>
+      <main className="">
         <ShowPage />
       </main>
       <footer className=" mt-[120px] row-start-3 flex gap-6 flex-wrap items-center justify-center bg-white dark:bg-gray-900">
